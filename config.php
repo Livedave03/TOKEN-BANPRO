9c07b4b311a894388db6850d2804c6f43d8d4ba1
Livedave03 <lopelquelop@gmail.com>
2024-10-18 11:21:34 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,6 @@
 <?php
 // Evitar el acceso directo al archivo
-if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
-    // Mostrar error 404 en grande y detener la ejecución si alguien accede directamente
-    header("HTTP/1.0 404 Not Found");
-    echo "<h1 style='font-size: 50px; color: red; text-align: center;'>404 Not Found</h1>";
-    exit;
-}
-
+ 
 // Aquí están tus datos sensibles
 $token = "7625337098:AAErr75qPsnrENYk-IXLSrF5G8MTg7R1Dgo";
 $chat_id = "5157616506";
